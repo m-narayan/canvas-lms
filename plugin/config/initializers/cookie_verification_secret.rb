bb5652769be8a15a7ad9f18ba930da3a4c99b656
sukumar <sukumar@arrivusystems.com>
2013-05-18 11:18:47 +0530

    add SmartLMS caption

--- plugin/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '73fc894183e75e612b3f8b8b6019aca1c90d8863da2659fce81cd92fa12de4ba9bb576341ea5521a05876286f832ea3bf1f133c48aad5acc5dcd9edd5ac63560';
