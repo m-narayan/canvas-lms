fe924572c55ca4293f0e16ec487ac568afc199c7
sukumar <sukumar@arrivusystems.com>
2013-06-12 11:46:34 +0530

    chatiframe

--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '4ab3808765744c13eb2a697534ba648cb730fb1958ee885bc8b60d57eb760548665515184d624ae2c4f95c408d6328585336f36183b5871b4b3efb6dbd1e0fcf';
