a493c534e320b8d5a041727afd2b204d235b4d2f
Sukumar <sukumar@arrivusystems.com>
2013-07-09 10:09:45 +0530

    demo controller

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -8,19 +8,16 @@ class DemoController < ApplicationController
   def create
     @demo = Demo.new(params[:demo])
     respond_to do |format|
-
     recap = Canvas::Plugin.find('registration_form_recaptcha')
     captcha_valid=true
     if recap && !recap.settings[:public_key].blank? && !recap.settings[:private_key].blank?
       captcha_valid=verify_recaptcha(:model => @demo, :private_key => recap.settings[:private_key])
     end
     if captcha_valid
-
         @account = Account.find_by_name(params[:demo][:organization])
-
         unless @account
-
           if @demo.save
+            password=(0...10).map{ ('a'..'z').to_a[rand(26)] }.join
             @account = Account.new
             @account.name = params[:demo][:organization]
             @account.save
@@ -30,21 +27,26 @@ class DemoController < ApplicationController
             user.register! unless user.registered?
             unless pseudonym
               pseudonym = user.pseudonyms.create!(:unique_id => params[:demo][:email],
-                                                  :password => "validpassword", :password_confirmation => "validpassword",
+                                                  :password => password, :password_confirmation => password,
                                                   :account => @account )
               user.communication_channels.create!(:path => params[:demo][:email]) { |cc| cc.workflow_state = 'active' }
             end
             pseudonym.save
             @account.add_user(user, 'AccountAdmin')
             reset_session_for_login
-            login={:unique_id =>params[:demo][:email],:password=>"validpassword",:remember_me=>"0"}
+            login={:unique_id =>params[:demo][:email],:password=>password,:remember_me=>"0"}
             @pseudonym_session = @account.pseudonym_sessions.new(login)
             @pseudonym_session.remote_ip = request.remote_ip
             @pseudonym_session.save
             format.html{ redirect_to dashboard_url}
-          else
 
-         format.html {render :action => "new"}
+            cc=CommunicationChannel.new
+            m=cc.messages.new
+            m.to = "params[:demo][:email]"
+            m.body = "hello"
+            Mailer.deliver_message(m) rescue nil
+          else
+            format.html {render :action => "new"}
           end
         else
           flash[:error] = "Organization Name Already Exists.Please choose a different name"
