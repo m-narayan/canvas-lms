9a7427021e0906db62a1d36260e6d98160300724
SaravananNV <saran@arrivusystems.com>
2013-07-19 10:32:31 +0530

    bundle update

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,8 @@
-server "beacon.arrivu.corecloud.com", :app, :web, :db, :primary => true
+#server "beacon.arrivu.corecloud.com", :app, :web, :db, :primary => true
+server "127.0.0.1", :app, :web, :db, :primary => true
 set :deploy_to, "/var/capistrano/beacon/lms_staging"
 set :rails_env, "staging" 
-set :branch,    "develop"
+set :branch,    "deploy"
 set :scm_passphrase, "deployadmin123$"
 #set :smart_lms_data_files "/var/capistrano/beacon/lms_staging/data/files"
 set :smart_lms_data_files, "#{deploy_to}/data/files"
