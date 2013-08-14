05a59a4b372b8ef369b272635e1f1b98ed986d53
natarajan1 <natarajan@arrivusystems.com>
2013-08-14 10:46:50 +0530

    commit to pull latest code for selenium test

--- spec/selenium/multi_tenancy_testcases/Site_admin_login_mt_spec.rb
+++ spec/selenium/multi_tenancy_testcases/Site_admin_login_mt_spec.rb
@@ -1,40 +1,70 @@
 require File.expand_path(File.dirname(__FILE__) + '/common_mt')
 
-describe "Site_Admin user creation test" do
+describe "Site_Admin user creation and Login test" do
 
   it_should_behave_like "in-process server selenium tests"
 
-  def fill_sms_settings_form(sms_url,username, password,sender_name)
-    url_element= f('#account_settings_OpenLMS_sms_url')
-    url_element.send_keys(sms_url)
-    username_element = f('#account_settings_OpenLMS_sms_user')
-    username_element.send_keys(username)
-    password_element = f('#account_settings_OpenLMS_sms_password')
-    password_element.send_keys(password)
-    sender_element=f('#account_settings_OpenLMS_sms_sender_name')
-    sender_element.send_keys(sender_name)
-    password_element.submit
-  end
 
-  account=create_site_admin("openlms","openlms@arrivusystems.com","Admin123$")
+  openlms_account=create_site_admin("openlms","openlms@arrivusystems.com","Admin123$")
+
   ibm_account=add_mt_account("ibm")
   add_mt_account_admin_users("ibm","ibm@arrivusystems.com","Admin123$")
-  course=create_course("IBM course M1-Sample Course1","IBMM1","ibm")
+
+  tcs_account=add_mt_account("tcs")
+  add_mt_account_admin_users("tcs","tcs@arrivusystems.com","Admin123$")
+
+  cisco_account=add_mt_account("cisco")
+  add_mt_account_admin_users("cisco","cisco@arrivusystems.com","Admin123$")
+
+  beacon_account=add_mt_account("beacon")
+  add_mt_account_admin_users("beacon","beacon@arrivusystems.com","Admin123$")
+
+  infosys_account=add_mt_account("infosys")
+  add_mt_account_admin_users("infosys","infosys@arrivusystems.com","Admin123$")
+
 
   before do
     @login_error_box_css = ".error_text:last"
   end
 
-  it "should create a admin user for openlms account and login with it" do
-    driver.get "http://openlms.lvh.me:#{$server_port}"
-    fill_in_login_form("openlms@arrivusystems.com","Admin123$")
-    f('.user_name').text.should == "openlms@arrivusystems.com"
-    driver.get "http://openlms.lvh.me:#{$server_port}/accounts/#{account.id}/settings"
-    driver.get "http://openlms.lvh.me:#{$server_port}"
-    driver.get "http://openlms.lvh.me:#{$server_port}/error_reports"
+  def site_admin_login(params={})
+    driver.get "http://#{params[:account_name]}.lvh.me:#{$server_port}"
+    fill_in_login_form(params[:user_name],params[:password])
+    f('.user_name').text.should == params[:user_name]
+    driver.get "http://#{params[:account_name]}.lvh.me:#{$server_port}/accounts/#{params[:account_id]}/settings"
+    f("#tab-sms-settings-link").should be_displayed
+    f("#tab-plugin-link").should be_displayed
+    f("#tab-sms-settings-link").click
+    sleep 2
+    f("#tab-plugin-link").click
+    sleep 2
+    driver.get "http://#{params[:account_name]}.lvh.me:#{$server_port}/accounts/#{params[:check_account_id]}/settings"
+    f("#section-tabs-header").text.should==params[:account_to_check]
+    f("#tab-sms-settings-link").should be_displayed
+    f("#tab-plugin-link").should be_displayed
+    f("#tab-sms-settings-link").click
+    sleep 2
+    f("#tab-plugin-link").click
+    sleep 2
     expect_new_page_load { f('.logout > a').click }
-
   end
+
+  it "should login as a site administrator and visit plugin settings in OPENLMS account" do
+    #driver.get "http://openlms.lvh.me:#{$server_port}"
+    #fill_in_login_form("openlms@arrivusystems.com","Admin123$")
+    #f('.user_name').text.should == "openlms@arrivusystems.com"
+    #driver.get "http://openlms.lvh.me:#{$server_port}/accounts/#{account.id}/settings"
+    #f("#tab-sms-settings-link").should be_displayed
+    #f("#tab-plugin-link").should be_displayed
+    #f("#tab-sms-settings-link").click
+    #sleep 2
+    #f("#tab-plugin-link").click
+    #sleep 2
+    #expect_new_page_load { f('.logout > a').click }
+    site_admin_login(:account_name=>"openlms",:account_id=>openlms_account.id,:account_to_check =>"ibm",:check_account_id=>ibm_account.id, :user_name=>"openlms@arrivusystems.com", :password=>"Admin123$")
+  end
+
+
 
   it "should site admin user login in IBM account" do
     driver.get "http://ibm.lvh.me:#{$server_port}"
@@ -44,59 +74,5 @@ describe "Site_Admin user creation test" do
     expect_new_page_load { f('.logout > a').click }
 
   end
-
-  it "should login as a site administrator and visit plugin settings" do
-    driver.get "http://openlms.lvh.me:#{$server_port}"
-    fill_in_login_form("openlms@arrivusystems.com","Admin123$")
-    f('.user_name').text.should == "openlms@arrivusystems.com"
-    driver.get "http://openlms.lvh.me:#{$server_port}/accounts/#{ibm_account.id}/settings"
-    f("#tab-plugin-link").click
-    driver.find_element(:id, 'account_settings_OpenLMS_kaltura_disable').click
-    driver.find_element(:id, 'account_settings_OpenLMS_sub_account_disable').click
-    driver.find_element(:id, 'account_settings_OpenLMS_grade_disable').click
-
-    #f("#account_settings_OpenLMS_kaltura_disable").click
-    #f("account_settings_OpenLMS_bbb_disable").click
-    #f("account_settings_OpenLMS_kandan_chat_disable").click
-    #f("account_settings_OpenLMS_google_docs_disable").click
-    #f("account_settings_OpenLMS_Crocodoc_disable").click
-    #f("account_settings_OpenLMS_grade_disable").click
-    #f("account_settings_OpenLMS_outcomes_disable").click
-    #f("account_settings_OpenLMS_sub_account_disable").click
-    #f("#btn_plugin_submit").click
-    submit_form("#account_plugin_settings")
-    #sleep(10)
-    #wait_for_ajax_requests
-
-    driver.get "http://openlms.lvh.me:#{$server_port}"
-    driver.get "http://openlms.lvh.me:#{$server_port}/accounts/#{ibm_account.id}/settings"
-    f("#tab-plugin-link").click
-    expect_new_page_load { f('.logout > a').click }
-
-
-    driver.get "http://ibm.lvh.me:#{$server_port}"
-    fill_in_login_form("ibm@arrivusystems.com","Admin123$")
-    f('.user_name').text.should == "ibm@arrivusystems.com"
-    driver.get "http://ibm.lvh.me:#{$server_port}/accounts/#{ibm_account.id}/settings"
-
-  end
-
-  it "should login as a site administrator and visit plugin settings" do
-    driver.get "http://openlms.lvh.me:#{$server_port}"
-    fill_in_login_form("openlms@arrivusystems.com","Admin123$")
-    f('.user_name').text.should == "openlms@arrivusystems.com"
-    driver.get "http://openlms.lvh.me:#{$server_port}/accounts/#{account.id}/settings"
-    f("#tab-sms-settings-link").should be_displayed
-    f("#tab-plugin-link").should be_displayed
-    f("#tab-sms-settings-link").click
-    sleep(5)
-    f("#tab-plugin-link").click
-    sleep(5)
-    expect_new_page_load { f('.logout > a').click }
-  end
-
-
-
-
 
 end
