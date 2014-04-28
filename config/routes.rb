746757fdfd6691d3a419f84446f4d2ea2cb6d110
SaravananNV <saran@arrivusystems.com>
2014-04-28 19:55:09 +0530

    created view for popular course

--- config/routes.rb
+++ config/routes.rb
@@ -1082,13 +1082,13 @@ routes.draw do
     end
 
     scope(:controller => :popular_courses) do
-      def ef_routes(context)
+      def et_routes(context)
         get "#{context}s/:#{context}_id/popular_courses", :action => :index, :path_name => "#{context}_popular_courses"
         post "#{context}s/:#{context}_id/popular_courses", :action => :create, :path_name => "#{context}_popular_courses_create"
         put "#{context}s/:#{context}_id/popular_courses/:popular_courses_id", :action => :update, :path_name => "#{context}_popular_courses_update"
         delete "#{context}s/:#{context}_id/popular_courses/:popular_courses_id", :action => :destroy, :path_name => "#{context}_popular_courses_delete"
       end
-      ef_routes("course")
+      et_routes("account")
     end
 
     #scope(:controller => :course_descriptions) do
--- vendor/plugins/e_learning/app/controllers/popular_courses_controller.rb
+++ vendor/plugins/e_learning/app/controllers/popular_courses_controller.rb
@@ -1,2 +1,27 @@
 class PopularCoursesController < ApplicationController
+
+  def index
+    respond_to do |format|
+      @courses = []
+      @account_courses = @domain_root_account.courses
+      @account_courses.each_with_index do |course, idx|
+        #attachment = Attachment.find(slider.account_slider_attachment_id)
+        @course_image = CourseImage.find(course.id)
+        @users = course.users.count
+        @course_tags = course.tags.count
+        image_attachment = Attachment.find(@course_image.course_image_attachment_id)
+        background_image_attchment = Attachment.find(@course_image.course_back_ground_image_attachment_id)
+        @course_json =   api_json(course, @current_user, session, API_USER_JSON_OPTS).tap do |json|
+          json[:id] = course.id
+          json[:course_name] = course.name
+          json[:course_image] = file_download_url(image_attachment, { :verifier => image_attachment.uuid, :download => '1', :download_frd => '1' })
+          json[:course_background_image] = file_download_url(background_image_attchment, { :verifier => background_image_attchment.uuid, :download => '1', :download_frd => '1' })
+          json[:users] = @users
+          json[:tags] = @course_tags
+        end
+        @courses << @course_json
+      end
+      format.json {render :json => @courses.to_json}
+    end
+  end
 end
