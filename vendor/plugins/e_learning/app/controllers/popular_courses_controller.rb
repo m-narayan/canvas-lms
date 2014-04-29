1e5fb9bd001182c103eb4cc52cae3e2e22ae2597
SaravananNV <saran@arrivusystems.com>
2014-04-29 18:02:41 +0530

    popular course view generated without tag design

--- vendor/plugins/e_learning/app/controllers/popular_courses_controller.rb
+++ vendor/plugins/e_learning/app/controllers/popular_courses_controller.rb
@@ -3,12 +3,22 @@ class PopularCoursesController < ApplicationController
   def index
     respond_to do |format|
       @courses = []
+      @total_tags = []
       @account_courses = @domain_root_account.courses
       @account_courses.each_with_index do |course, idx|
         #attachment = Attachment.find(slider.account_slider_attachment_id)
         @course_image = CourseImage.find(course.id)
-        @users = course.users.count
-        @course_tags = course.tags.count
+        @users_count = course.users.count
+        @course_tags_count = course.tags.count
+        @course_tags = course.tags
+        if course.course_description
+          @course_desc = CourseDescription.find(course.id)
+          @short_course_desc = @course_desc.long_description
+        end
+        @course_tags.each_with_index do |tag, id|
+
+          @total_tags << tag
+        end
         image_attachment = Attachment.find(@course_image.course_image_attachment_id)
         background_image_attchment = Attachment.find(@course_image.course_back_ground_image_attachment_id)
         @course_json =   api_json(course, @current_user, session, API_USER_JSON_OPTS).tap do |json|
@@ -16,12 +26,17 @@ class PopularCoursesController < ApplicationController
           json[:course_name] = course.name
           json[:course_image] = file_download_url(image_attachment, { :verifier => image_attachment.uuid, :download => '1', :download_frd => '1' })
           json[:course_background_image] = file_download_url(background_image_attchment, { :verifier => background_image_attchment.uuid, :download => '1', :download_frd => '1' })
-          json[:users] = @users
-          json[:tags] = @course_tags
+          json[:users_count] = @users_count
+          json[:tags_count] = @course_tags_count
+          json[:course_tags] = @total_tags
+          json[:course_short_decription] = @short_course_desc
         end
+        @total_tags = []
         @courses << @course_json
       end
       format.json {render :json => @courses.to_json}
     end
   end
+
+
 end
